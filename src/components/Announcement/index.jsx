5f27f74ca875659b8730197fba8f180788f48d73
thanhson3133 <thanhson3133@gmail.com>
2021-10-28 11:00:52 +0700

    design

---
+++ src/components/Announcement/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+    align-items: center;
+`
+
+const Announcement = () => {
+    return (
+        <Container>
+            Super Deal! Free Shipping on Orders Over $50
+        </Container>
+    );
+}
+
+export default Announcement;
